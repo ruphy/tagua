7f1a4bcc443239076a221b07a2676b2f62484de7
Maurizio Monge <maurizio@localhost.localdomain>
2007-07-07 16:45:01 +0100

    Forgotten to update this sprite.h in previous commit

--- src/sprite.h
+++ src/sprite.h
@@ -1,7 +1,7 @@
 /*
   Copyright (c) 2006 Paolo Capriotti <p.capriotti@sns.it>
             (c) 2006 Maurizio Monge <maurizio.monge@kdemail.net>
-            
+
   This program is free software; you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
   the Free Software Foundation; either version 2 of the License, or
@@ -123,7 +123,5 @@ public:
   void hide(){ m_dummy_visible=false; update_from_dummy(); }
 #endif
 };
-
-typedef boost::shared_ptr<Sprite> SpritePtr;
 
 #endif // SPRITE_H
