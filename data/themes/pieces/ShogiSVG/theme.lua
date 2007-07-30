2f92d93c6a196a44fe53d37eb7f478fdac78f5c9
Yann Dirson <ydirson@altern.org>
2007-07-31 00:57:41 +0200

    Resurrect ShogiSVG theme and make it use the shogi lib.

--- data/themes/pieces/ShogiSVG/theme.lua
+++ data/themes/pieces/ShogiSVG/theme.lua
@@ -1,46 +1,54 @@
-import("piece_theme.lua")--
--- shadow=7.0
--- shadow_color="#404050"
--- shadow_offset_x=6
--- shadow_offset_y=4
--- shadow_grow=5
---
--- function fromSVG(file)
---   return function(size)
---     local i = Image(size,size)
---     i:clear()
---     i:draw_svg(Rect(0,0,size,size), "tile.svg")
---     i:draw_svg(Rect(0,0,size,size), file)
---     return i
---   end
--- end
---
--- function fromSVG_ROT(file)
---   return function(size)
---     local i = Image(size,size)
---     i:clear()
---     i:translate(size,size)
---     i:rotate(180)
---     i:draw_svg(Rect(0,0,size,size), "tile.svg")
---     i:draw_svg(Rect(0,0,size,size), file)
---     return i
---   end
--- end
---
--- theme.black_bishop = addShadow(fromSVG_ROT("bishop.svg"))
--- theme.black_king   = addShadow(fromSVG_ROT("king.svg"))
--- theme.black_knight = addShadow(fromSVG_ROT("horse.svg"))
--- theme.black_pawn   = addShadow(fromSVG_ROT("pawn.svg"))
--- theme.black_queen  = addShadow(fromSVG_ROT("gold.svg"))
--- theme.black_rook   = addShadow(fromSVG_ROT("rook.svg"))
--- theme.white_bishop = addShadow(fromSVG("bishop.svg"))
--- theme.white_king   = addShadow(fromSVG("king.svg"))
--- theme.white_knight = addShadow(fromSVG("horse.svg"))
--- theme.white_pawn   = addShadow(fromSVG("pawn.svg"))
--- theme.white_queen  = addShadow(fromSVG("gold.svg"))
--- theme.white_rook   = addShadow(fromSVG("rook.svg"))
---
--- name = "Shogi"
--- description = "Shogi SVG theme,\n  generate with potrace"
--- variants = { "Chess[default]" }
+import("piece_theme.lua")
+import("shogi_themelib.lua")
 
+shadow=7.0
+shadow_color="#404050"
+shadow_offset_x=6
+shadow_offset_y=4
+shadow_grow=5
+
+function addGlyph(file)
+  return function(i, size)
+    i:draw_svg(Rect(0,0,size,size), file)
+    return i
+  end
+end
+
+function shogi_piece(file, white, ratio)
+  return addShadow(overlay(tile(white, ratio),
+			   addGlyph(file)))
+end
+
+theme.black_king     = shogi_piece("king.svg", false, 1)
+theme.black_rook     = shogi_piece("rook.svg", false, 0.96)
+theme.black_p_rook   = shogi_piece("prook.svg", false, 0.96)
+theme.black_bishop   = shogi_piece("bishop.svg", false, 0.93)
+theme.black_p_bishop = shogi_piece("pbishop.svg", false, 0.93)
+theme.black_gold     = shogi_piece("gold.svg", false, 0.9)
+theme.black_silver   = shogi_piece("silver.svg", false, 0.9)
+theme.black_p_silver = shogi_piece("psilver.svg", false, 0.9)
+theme.black_knight   = shogi_piece("horse.svg", false, 0.86)
+theme.black_p_knight = shogi_piece("phorse.svg", false, 0.86)
+theme.black_lance    = shogi_piece("lance.svg", false, 0.83)
+theme.black_p_lance  = shogi_piece("plance.svg", false, 0.83)
+theme.black_pawn     = shogi_piece("pawn.svg", false, 0.8)
+theme.black_p_pawn   = shogi_piece("ppawn.svg", false, 0.8)
+
+theme.white_king     = shogi_piece("king.svg", true, 1)
+theme.white_rook     = shogi_piece("rook.svg", true, 0.96)
+theme.white_p_rook   = shogi_piece("prook.svg", true, 0.96)
+theme.white_bishop   = shogi_piece("bishop.svg", true, 0.93)
+theme.white_p_bishop = shogi_piece("pbishop.svg", true, 0.93)
+theme.white_gold     = shogi_piece("gold.svg", true, 0.9)
+theme.white_silver   = shogi_piece("silver.svg", true, 0.9)
+theme.white_p_silver = shogi_piece("psilver.svg", true, 0.9)
+theme.white_knight   = shogi_piece("horse.svg", true, 0.86)
+theme.white_p_knight = shogi_piece("phorse.svg", true, 0.86)
+theme.white_lance    = shogi_piece("lance.svg", true, 0.83)
+theme.white_p_lance  = shogi_piece("plance.svg", true, 0.83)
+theme.white_pawn     = shogi_piece("pawn.svg", true, 0.8)
+theme.white_p_pawn   = shogi_piece("ppawn.svg", true, 0.8)
+
+-- To be able to adapt this theme to chess too
+theme.black_queen    = shogi_piece("gold.svg", false, 0.9)
+theme.white_queen    = shogi_piece("gold.svg", true, 0.9)
