e96ad07b62c1dd8954f9d29bf8f8ad955a216240
Paolo Capriotti <paolo@snare.(none)>
2007-08-14 11:27:17 +0200

    Chess actions are now checkable.

--- src/hlvariant/chess/variant.cpp
+++ src/hlvariant/chess/variant.cpp
@@ -2,6 +2,7 @@
 #include "../tagua_wrapped.h"
 #include "actioncollection.h"
 
+#include <QActionGroup>
 #include <KAction>
 
 namespace HLVariant {
@@ -10,11 +11,24 @@ namespace Chess {
 const char* Variant::m_name = "Chess";
 const char* Variant::m_theme_proxy = "Chess";
 
+QAction* createAction(QActionGroup* group, ActionCollection* actions, 
+                      const KIcon& icon, const QString& text) {
+  QAction* action = new KAction(icon, text, group);
+  action->setCheckable(true);
+  group->addAction(action);
+  actions->add(action);
+  return action;
+}
+
 void Variant::setupActions(ActionCollection* actions) {
-  actions->add(new KAction(KIcon("promoteQueen"), "Promote to queen", actions));
-  actions->add(new KAction(KIcon("promoteRook"), "Promote to rook", actions));
-  actions->add(new KAction(KIcon("promoteBishop"), "Promote to bishop", actions));
-  actions->add(new KAction(KIcon("promoteKnight"), "Promote to knight", actions));
+  QActionGroup* group = new QActionGroup(actions);
+  
+  QAction* queen = createAction(group, actions, KIcon("promoteQueen"), "Promote to queen");
+  createAction(group, actions, KIcon("promoteRook"), "Promote to rook");
+  createAction(group, actions, KIcon("promoteBishop"), "Promote to bishop");
+  createAction(group, actions, KIcon("promoteKnight"), "Promote to knight");
+  
+  queen->setChecked(true);
 }
 
 } // namespace Chess
