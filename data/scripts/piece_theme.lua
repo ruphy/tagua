ruphy/tagua

commit f4afb536fcba4c035e0c9cc1d3d06577c007895e

Fix orientation after flip.
Pieces are now rerendered when the board is flipped, and the necessary
information is passed to the theme, so that it can pick the right
orientation.

Since the animation had become sloppy (pieces were rerendered and animated
at the same time), I have disabled it.

Fixes #45.

data/scripts/piece_theme.lua
@@ -1,10 +1,10 @@
 
 function addShadow(func)
-  return function(size)
+  return function(size, args)
     local isz = math.floor( size*100/(100+shadow_grow) + 0.5)
     local offx = shadow_offset_x*isz/200
     local offy = shadow_offset_y*isz/200
-    local i = func(isz)
+    local i = func(isz, args)
     local s = i:create_shadow( shadow*isz/100, shadow_color,
                                   Point(size-isz, size-isz),
                                   Point(offx, offy) )
@@ -14,7 +14,7 @@ function addShadow(func)
 end
 
 function fromSVG_Direct(file)
-  return function(size)
+  return function(size, args)
     local i = Image(size,size)
     i:clear()
     i:draw_svg(Rect(0,0,size,size), file)
@@ -32,7 +32,7 @@ end
 
 function fromFontGlyph_Direct(...)
   local t = (function(...) return ... end):partial(...)
-  return function(size)
+  return function(size, args)
     local i = Image(size,size)
     i:clear()
     i:draw_glyph(Rect(0,0,size,size), t())
@@ -58,7 +58,7 @@ function fromFontGlyph(...)
 end
 
 function fromPixmap(file)
-  return function(size)
+  return function(size, args)
     local i = Image(size,size)
     i:set_paint_over(false)
     i:draw_image(Rect(0,0,size,size), file)
@@ -67,7 +67,7 @@ function fromPixmap(file)
 end
 
 function fromColor(color)
-  return function(size)
+  return function(size, args)
     local i = Image(size,size)
     i:clear(color)
     return i
@@ -76,9 +76,9 @@ end
 
 function overlay(func1,func2,...)
    if func2 then
-      return overlay(function(size)
-			return func2(func1(size), size)
-		     end, ...)
+      return overlay(function(size, args)
+        return func2(func1(size, args), size)
+          end, ...)
    else
       return func1
    end

data/scripts/shogi_themelib.lua
@@ -2,7 +2,7 @@
 -- FIXME: still depends on Shogi.ttf for the tile
 
 function tile(white, ratio)
-  return function(size)
+  return function(size, args)
     local i = Image(size,size)
     i:clear()
 
@@ -11,7 +11,9 @@ function tile(white, ratio)
     g[1] = "#c0a870"
     local b = Brush(g)
 
-    if white then
+    local flip = (white or args.flipped) and not (white and args.flipped)
+
+    if flip then
       i:translate(-size, -size)
       i:rotate(180)
       b:translate(-size, -size)

src/board.cpp
@@ -319,7 +319,7 @@ void Board::updateSprites() {
 
     if (p) {
       // drawing sprite
-      p->setPixmap( m_loader( m_sprites[i].name() ) );
+      p->setPixmap(m_loader.piecePixmap(m_sprites[i].name(), m_flipped));
       adjustSprite(i, true);
     }
   }
@@ -646,9 +646,13 @@ void Board::flip(bool flipped)
     m_flipped = flipped;
 
     // update sprite positions
-    for (Point i = m_sprites.first(); i <= m_sprites.last(); i = m_sprites.next(i))
-    if (m_sprites[i].sprite())
-      adjustSprite(i);
+    for (Point i = m_sprites.first(); i <= m_sprites.last(); i = m_sprites.next(i)) {
+      SpritePtr p = m_sprites[i].sprite();
+      if (p) {
+        p->setPixmap(m_loader.piecePixmap(m_sprites[i].name(), m_flipped));
+        adjustSprite(i, true);
+      }
+    }
 
     updateTags();
     updateBorder();

src/pixmaploader.cpp
@@ -103,6 +103,15 @@ QPixmap PixmapLoader::operator()(const QString& id) {
   return getValue<QPixmap>(id);
 }
 
+QPixmap PixmapLoader::piecePixmap(const QString& id, bool flipped) {
+  ::LuaApi::LuaValueMap args;
+  if (flipped)
+    args["flipped"] = 0.0;
+
+  return getValue<QPixmap>(id, &args);
+//   return getValue<QPixmap>(id);
+}
+
 template<typename T>
 T PixmapLoader::getValue(const QString& id, const ::LuaApi::LuaValueMap* args, bool allow_nil) {
   if (!m_size || !m_theme)

src/pixmaploader.h
@@ -67,6 +67,8 @@ public:
 
   /** looks up a string id (for instance a predefined id, like "background" or "highlighting") */
   QPixmap operator()(const QString& id);
+  
+  QPixmap piecePixmap(const QString& id, bool flipped = false);
 
   /** returns a value */
   template<typename T>

commit cca85af350a95b969ac1bb5ffd0b0893e7f97f79

Fixed bug introduced by commit f4afb53.
In some situations sprites where not loaded and failed to appear. This was
because the 'args' parameter was not correctly forwarded to the lua rendering
function by some calls to the loader object (specifically Theme::operator()).

I have now renamed operator() to the more meaningful getPixmap, and replaced
all calls that retrieve a piece sprites with an invocation of pieceSprite.

Convenience functions Board::loadSprite and PiecePool::loadSprite have been
added, that automatically load the sprite with the correct orientation
depending on the flipped state.

data/scripts/piece_theme.lua
@@ -77,7 +77,7 @@ end
 function overlay(func1,func2,...)
    if func2 then
       return overlay(function(size, args)
-        return func2(func1(size, args), size)
+        return func2(func1(size, args), size, args)
           end, ...)
    else
       return func1

data/scripts/shogi_themelib.lua
@@ -10,7 +10,7 @@ function tile(white, ratio)
     g[0] = "#ffeda2"
     g[1] = "#c0a870"
     local b = Brush(g)
-
+    
     local flip = (white or args.flipped) and not (white and args.flipped)
 
     if flip then

src/board.cpp
@@ -133,7 +133,7 @@ boost::shared_ptr<KGameCanvasPixmap> Board::addTag(const QString& name, Point pt
   if(!m_sprites.valid(pt))
     return boost::shared_ptr<KGameCanvasPixmap>();
 
-  QPixmap p = m_tags_loader(name);
+  QPixmap p = m_tags_loader.getPixmap(name);
   boost::shared_ptr<KGameCanvasPixmap> item =
       boost::shared_ptr<KGameCanvasPixmap>(new KGameCanvasPixmap(p, this));
   item->moveTo(converter()->toReal(pt));
@@ -262,6 +262,10 @@ void Board::createGrid(Point p, const QStringList& border_coords) {
   recreateBorder();
 }
 
+QPixmap Board::loadSprite(const QString& id) {
+  return m_loader.piecePixmap(id, m_flipped);
+}
+
 QRect Board::computeRect(Point p) const {
   QPoint realPoint = converter()->toReal(p);
   return squareRect(realPoint.x(), realPoint.y());
@@ -319,7 +323,7 @@ void Board::updateSprites() {
 
     if (p) {
       // drawing sprite
-      p->setPixmap(m_loader.piecePixmap(m_sprites[i].name(), m_flipped));
+      p->setPixmap(loadSprite(m_sprites[i].name()));
       adjustSprite(i, true);
     }
   }
@@ -333,7 +337,7 @@ void Board::updateTags() {
   for(std::map<Point, boost::shared_ptr<KGameCanvasPixmap> >::iterator pt =
                           tit->second.begin(); pt != tit->second.end(); ++pt) {
     pt->second->moveTo(converter()->toReal(pt->first));
-    pt->second->setPixmap(m_tags_loader(tit->first));
+    pt->second->setPixmap(m_tags_loader.getPixmap(tit->first));
   }
 }
 
@@ -601,7 +605,7 @@ void Board::updateHinting(Point pt, AbstractPiece::Ptr piece) {
     if(pt == m_hinting_pos) {
       if(!(piece->name() == m_hinting.name())) {
         m_hinting = NamedSprite(piece->name(), m_hinting.sprite());
-        m_hinting.sprite()->setPixmap(m_loader(piece->name()));
+        m_hinting.sprite()->setPixmap(loadSprite(piece->name()));
       }
     }
     else {
@@ -613,7 +617,7 @@ void Board::updateHinting(Point pt, AbstractPiece::Ptr piece) {
           enqueue( boost::shared_ptr<Animation>(new CaptureAnimation(m_hinting.sprite())) );
       }
 
-      QPixmap pix = m_loader(piece->name());
+      QPixmap pix = loadSprite(piece->name());
       SpritePtr sprite(new Sprite(pix, piecesGroup(), converter()->toReal(pt)));
       sprite->setOpacity(160);
       sprite->raise();

src/board.h
@@ -226,6 +226,9 @@ public:
 
   /** returns the sprite loader */
   PixmapLoader* loader() { return &m_loader; }
+  
+  /** Load a sprite using the sprite loader */
+  QPixmap loadSprite(const QString& id);
 
   /** returns the sprite loader */
   const PixmapLoader* loader() const { return &m_loader; }

src/graphicalsystem.cpp
@@ -119,7 +119,7 @@ NamedSprite GraphicalSystem::createPiece(const Point& p, const AbstractPiece* pi
   if (!m_board->m_sprites.valid(p))
     return NamedSprite();
 
-  NamedSprite s(piece->name(), SpritePtr(new Sprite(m_board->m_loader(piece->name()),
+  NamedSprite s(piece->name(), SpritePtr(new Sprite(m_board->loadSprite(piece->name()),
                                  m_board->piecesGroup(), m_board->converter()->toReal(p))) );
   if (show)
     s.sprite()->show();
@@ -151,7 +151,8 @@ NamedSprite GraphicalSystem::takePoolSprite(int pool, int index) {
 
 NamedSprite GraphicalSystem::insertPoolPiece(int pool, int index, const AbstractPiece* piece) {
   PiecePool *pl = m_view->pool(pool);
-  QPixmap px = pl->m_loader(piece->name());
+  QPixmap px = pl->loadSprite(piece->name());
+//   QPixmap px = pl->m_loader(piece->name());
 
   NamedSprite s( piece->name(), SpritePtr( new Sprite( px, pl, QPoint() ) ) );
   pl->insertSprite(index, s);

src/luaapi/loader.cpp
@@ -316,7 +316,6 @@ int Loader::create_value_func(lua_State *l) {
 
   // If it is a function, call it
   if(lua_isfunction(l, -1)) {
-//     std::cout << "CALLING FUNCTION" << std::endl;
     int nparams = 0;
     if(data->size) {
       lua_pushnumber(l, data->size);

src/piecepool.cpp
@@ -212,7 +212,7 @@ void PiecePool::cancelDragging(bool fadeOff) {
   }
 
   m_dragged.sprite()->putInCanvas(this);
-  m_dragged.sprite()->setPixmap( m_loader( m_dragged.name() ) );
+  m_dragged.sprite()->setPixmap(loadSprite(m_dragged.name()));
   insertSprite(m_dragged_index, m_dragged);
 
   m_dragged = NamedSprite();
@@ -239,7 +239,7 @@ void PiecePool::onResize(int new_size, bool force_reload) {
 
   // update the sprites
   for (int i=0;i<(int)m_sprites.size(); i++) {
-    m_sprites[i].sprite()->setPixmap( m_loader( m_sprites[i].name() ) );
+    m_sprites[i].sprite()->setPixmap(loadSprite(m_sprites[i].name()));
     m_main_animation->addAnimation(AnimationPtr(new InstantAnimation(m_sprites[i].sprite(), toReal(i))));
   }
 }
@@ -281,7 +281,7 @@ void PiecePool::onMousePress(const QPoint& pos, int button) {
   got.sprite()->hide();
 
   /* recreate the sprite, as "got" may be being animated */
-  QPixmap px = m_board->m_loader( got.name() );
+  QPixmap px = m_board->loadSprite(got.name());
   QPoint at = pos + this->pos() - m_board->pos() - QPoint(px.width(), px.height())/2;
   m_dragged = NamedSprite(  got.name(), SpritePtr(new Sprite(px, m_board->piecesGroup(),  at) ) );
   m_dragged.sprite()->raise();
@@ -298,3 +298,8 @@ void PiecePool::onMouseMove(const QPoint& pos, int /*button*/) {
     m_board->draggingOn( m_pool_num, m_dragged_index, pos + this->pos() - m_board->pos() );
   }
 }
+
+QPixmap PiecePool::loadSprite(const QString& id) {
+  return m_loader.piecePixmap(id, m_flipped);
+}
+

src/piecepool.h
@@ -95,6 +95,9 @@ public:
 
   /** returns the sprite loader */
   const PixmapLoader* loader() const { return &m_loader; }
+  
+  /** Load a sprite using the sprite loader */
+  QPixmap loadSprite(const QString& id);
 
   /** returns the flipped value */
   bool flipped() const { return m_flipped; }

src/pixmaploader.cpp
@@ -99,7 +99,7 @@ void PixmapLoader::initialize() {
   m_loader->refSize(0);
 }
 
-QPixmap PixmapLoader::operator()(const QString& id) {
+QPixmap PixmapLoader::getPixmap(const QString& id) {
   return getValue<QPixmap>(id);
 }
 

src/pixmaploader.h
@@ -66,7 +66,8 @@ public:
   void setSize(int s);
 
   /** looks up a string id (for instance a predefined id, like "background" or "highlighting") */
-  QPixmap operator()(const QString& id);
+//   QPixmap operator()(const QString& id);
+  QPixmap getPixmap(const QString& id);
   
   QPixmap piecePixmap(const QString& id, bool flipped = false);
 