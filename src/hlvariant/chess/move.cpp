ea2ce1e6e9fa13aae288a24118e69e9f04748ffd
Paolo Capriotti <p.capriotti@gmail.com>
2007-11-11 09:57:58 +0100

    Add copyright statements where missing.

--- src/hlvariant/chess/move.cpp
+++ src/hlvariant/chess/move.cpp
@@ -1,3 +1,13 @@
+/*
+  Copyright (c) 2007 Paolo Capriotti <p.capriotti@gmail.com>
+            (c) 2007 Maurizio Monge <maurizio.monge@kdemail.net>
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+*/
+
 #include "move.h"
 
 namespace HLVariant {
